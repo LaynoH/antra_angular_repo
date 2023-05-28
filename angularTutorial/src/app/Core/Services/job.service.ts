fa26795a3b122835d111796a3669e7b7171c2e8f
elainechiang <elainechiang99@gmail.com>
2023-05-28 16:53:48 -0400

    job.service update URL and key

--- angularTutorial/src/app/Core/Services/job.service.ts
+++ angularTutorial/src/app/Core/Services/job.service.ts
@@ -12,9 +12,9 @@ export class JobService {
 
   getAllJobs():Observable<Job[]>{
     let header = new HttpHeaders();
-    header = header.set('key','key_value');
-    return this.http.get<Job[]>("url_from_postman",{
-      headers: {'key':'key_value'}
+    header = header.set('Ocp-Apim-Subscription-Key','b7e325cf50db42b0b60a5d112390a6c3');
+    return this.http.get<Job[]>("https://recruitingapi.azure-api.net/recruiting/api/Jobs",{
+      headers: {'Ocp-Apim-Subscription-Key':'b7e325cf50db42b0b60a5d112390a6c3'}
     });
   }
 }
